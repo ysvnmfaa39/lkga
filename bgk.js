67de7d5ad3767e7fdd83fc533b4fdfd6fd988b4d
ysvnmfaa39 <87510545+ysvnmfaa39@users.noreply.github.com>
2021-07-16 00:11:59 -0400

    Create bgk.js

---
+++ bgk.js
@@ -0,0 +1,11 @@
+function load_home () {
+    fetch("/" /*, options */)
+    .then((response) => response.text())
+    .then((html) => {
+        document.querySelector("html").innerHTML = html;
+    })
+    .catch((error) => {
+        console.warn(error);
+    });
+}
+load_home();
